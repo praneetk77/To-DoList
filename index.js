f2470762a0f59b055eab7df20ae9e94674fdbd35
praneetk77 <praneetkarna77@gmail.com>
2021-08-06 21:23:52 +0545

    Add work list and about page

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
 
-var items = [];
+var items = ["Buy food", "Cook food", "Eat food"];
+var workItems = ["Code"];
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -16,12 +18,30 @@ app.get("/", function (req, res) {
     month: "long",
   };
   var day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, newItems: items });
+  res.render("list", { listTitle: day, newItems: items });
 });
 
 app.post("/", function (req, res) {
+  if (req.body.submit === "Work") {
+    workItems.push(req.body.newTask);
+    res.redirect("/work");
+  } else {
+    items.push(req.body.newTask);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work", newItems: workItems });
+});
+
+app.post("/work", function (req, res) {
   items.push(req.body.newTask);
-  res.redirect("/");
+  res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
