8efd78768de2c664fcf3def6c281991bfcd56bbe
praneetk77 <praneetkarna77@gmail.com>
2021-08-06 22:17:24 +0545

    Add seperate module for date

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/views/date.js");
 
 const app = express();
 
@@ -11,13 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = date.getDate();
   res.render("list", { listTitle: day, newItems: items });
 });
 
